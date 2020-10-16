23dbbbae1444ac48a9ac4339ae3f06627eb9fb5f
Marko Lazin <free@frees-iMac-4.local>
2020-10-15 20:09:25 -0400

    case study 6 commit added ggplot & table, tinkered w/ a few parts of initial script

--- week_06/GEO511_CS_06.R
+++ week_06/GEO511_CS_06.R
@@ -17,16 +17,30 @@ tmax_month<-getData(name="worldclim",var="tmax",res=10)
 gain(tmax_month)<-.1
 
 countries_world<-world%>%
-  filter(!continent=="Antarctica")
+  filter(!continent=="Antarctica")%>%
+  filter(!continent=="Seven seas (open ocean)")
 
 object_countries<-as(countries_world,"Spatial")
 plot(tmax_month$tmax2)
-
-#gain function
 
 tmax_annual<-max(tmax_month)
 plot(tmax_annual)
 names(tmax_annual)<-"tmax"
 
 tmax_country=raster::extract(tmax_annual,countries_world,fun=max,na.rm=TRUE,small=TRUE,sp=TRUE)
-#st_as_sf(tmax_country)
+tmax_country_sf=st_as_sf(tmax_country)
+tmax_country_sf
+
+tmax_country_map<-ggplot(tmax_country_sf)+
+  geom_sf(aes(fill=tmax))+
+  scale_fill_viridis_c(name="Annual Maximum\nTemperature °C")+
+  theme(legend.position='bottom',legend.title=element_text(size=16,color='orange'))
+tmax_country_map
+
+tmax_table<-as.data.frame(tmax_country_sf%>%
+  group_by(continent)%>%
+  top_n(1,tmax))
+keep<-c("name_long","continent","tmax")
+tmax_final<-tmax_table[,(names(tmax_table)%in%keep)]%>%
+  arrange(desc(tmax))
+tmax_final
